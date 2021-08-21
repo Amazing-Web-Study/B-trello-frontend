970631307888d1a4d6b6feeab6ed95989d1e114b
choi suill <gongdal5@naver.com>
2021-08-21 17:15:51 +0900

    useReducer를 사용하는 api 호출 방식

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,53 @@
-export default 'aa'
+import { useReducer, useEffect } from 'react'
+
+function reducer(state:any, action:any) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: [],
+                error: ''
+            }
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: ''
+            }
+        case 'ERROR':
+            return {
+                loading: false,
+                data: [],
+                error: '에러 발생'
+            }
+        default:
+            throw new Error(action.type)
+    }
+}
+
+function useFetch(callback:any, deps = []) {
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: [],
+        error: ''
+    })
+
+    const fetchData = async () => {
+        dispatch({type: 'LOADING'})
+        try {
+            const response = await callback()
+            let data = await response.json()
+            dispatch({type: 'SUCCESS', data: await data})
+        } catch (e) {
+            dispatch({type: 'ERROR', error: '에러 발생'})
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, deps)
+
+    return [state, fetchData]
+}
+
+export default useFetch
--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,31 +1,59 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 import MainCard from "../main/MainCard";
 // import {cards} from "../../api/cards";
 
 
+function reducer(state:any, action:any) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: [],
+                error: ''
+            }
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: ''
+            }
+        case 'ERROR':
+            return {
+                loading: false,
+                data: [],
+                error: '에러 발생'
+            }
+        default:
+            throw new Error(action.type)
+    }
+}
+
 function Main() {
-    const [cards, setCards] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState('')
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: [],
+        error: ''
+    })
+
+    const fetchCards = async () => {
+        dispatch({type: 'LOADING'})
+        try {
+            const response = await fetch('http://localhost:8080/api/card')
+            let data = await response.json()
+            dispatch({type: 'SUCCESS', data: await data})
+        } catch (e) {
+            dispatch({type: 'ERROR', error: '에러 발생'})
+        }
+    }
+    // const [cards, setCards] = useState([])
+    // const [loading, setLoading] = useState(false)
+    // const [error, setError] = useState('')
 
     useEffect(() => {
-        const fetchCards = async () => {
-            setError('')
-            setCards([])
-            setLoading(true)
-
-            const response:any = await fetch('http://localhost:8080/api/card')
-            let jsonResponse = await response.json()
-            setCards(jsonResponse)
-
-            setLoading(false)
-        }
         fetchCards()
-        fetchCards().catch((err:any) => {
-            setLoading(false)
-            setError('에러 발생')
-        })
     }, [])
+
+    const {loading, data: cards, error} = state
 
     if (loading) return <div>로딩중</div>
     if (error) return <div>{error}</div>
